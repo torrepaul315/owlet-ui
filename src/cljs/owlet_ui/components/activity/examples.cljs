e9ca5343e17d930aa6697f673f106c0627bbeb3d
dviramontes <dviramontes@gmail.com>
2016-12-06 08:36:53 -0500

    adds dropzone.css, init examples dropzone.js component

--- src/cljs/owlet_ui/components/activity/examples.cljs
+++ src/cljs/owlet_ui/components/activity/examples.cljs
@@ -1,15 +1,18 @@
 (ns owlet-ui.components.activity.examples
-  (:require [cljsjs.dropzone :as Dropzone]
-            [reagent.core :as reagent]))
+  (:require [cljsjs.dropzone]
+            [reagent.core :as reagent]
+            [reagent.debug :refer-macros [dbg]]))
 
 (defn examples-component []
   (reagent/create-class
-    {:component-will-mount
-     (fn [])
+    {:component-did-mount
+     (fn []
+       ;; disable auto discover of form.dropzone
+       (set! (.-autoDiscover js/Dropzone) false)
+       (js/Dropzone. ".dropzone" #js {:url "/foobar"}))
      :reagent-render
      (fn []
        [:div.activity-examples-wrap.box-shadow
         "example component"
         [:form {:id "my-awesome-dropzone"
-                :class "dropzone"
-                :action "/foo"}]])}))
+                :class "dropzone"}]])}))
